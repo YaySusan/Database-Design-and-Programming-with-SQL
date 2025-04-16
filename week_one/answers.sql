a838e385bb9ecc33bc4b66bcd8f8552a96c8a662
YaySusan <jonassusan13@gmail.com>
2025-04-16 13:08:43 +0200

    Week one folder and answers.sql assignment one.

---
+++ week_one/answers.sql
@@ -0,0 +1,5 @@
+-- 1. Write an SQL query to create a new database called 'salesDB'
+CREATE DATABASE salesDB;
+
+-- 2. Write an SQL query to drop (delete) the database called 'demo'
+DROP DATABASE demo;
